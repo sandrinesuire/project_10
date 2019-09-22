76ee81bcc7d7877e57ac63d27e43ef60e14b7fb3
sandrinesuire <sandrinesuire@me.com>
2019-09-22 17:03:50 +0200

    fix http  url

--- nutella/settings.py
+++ nutella/settings.py
@@ -23,7 +23,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = ['thebestsubstitute.herokuapp.com']
+ALLOWED_HOSTS = ['thebestsubstitute.herokuapp.com', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
