acecf452a759da54c508e4366b4380de6e526121
sandrinesuire <sandrinesuire@me.com>
2019-09-22 17:32:36 +0200

    mode debug

--- nutella/settings.py
+++ nutella/settings.py
@@ -23,7 +23,7 @@ if os.environ.get('ENV') == 'PRODUCTION':
 else:
     DEBUG = True
 
-ALLOWED_HOSTS = ['thebestsubstitute.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['0.0.0.0', 'thebestsubstitute.herokuapp.com', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
