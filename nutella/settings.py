0569d1701412de3a32a44651f385240ff2e69bd1
sandrinesuire <sandrinesuire@me.com>
2019-09-24 18:09:30 +0200

    settings mode production

--- nutella/settings.py
+++ nutella/settings.py
@@ -21,7 +21,7 @@ SECRET_KEY = '+wgbpb9yvdc@3z-)2m9=dyyvigm8px$(d=j-_y+kn45h)u8h95'
 if os.environ.get('ENV') == 'PRODUCTION':
     DEBUG = True
 else:
-    DEBUG = True
+    DEBUG = False
 
 ALLOWED_HOSTS = ['thebestsubstitute.herokuapp.com', '127.0.0.1']
 
