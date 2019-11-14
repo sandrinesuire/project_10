251d3b9ce9b4c9746ab09afa54366f9df59555ac
sandrinesuire <suiresandrine1@gmail.com>
2019-11-14 21:59:08 +0100

    test travis build

--- nutella/settings/travis.py
+++ nutella/settings/travis.py
@@ -1,6 +1,6 @@
 from . import *
 
-SECRET_KEY = '+wgbpb9yvdc@3z-)2m9=dyyvigm8px$(d=j-_y+kn45h)u8h95'
+# SECRET_KEY = '+wgbpb9yvdc@3z-)2m9=dyyvigm8px$(d=j-_y+kn45h)u8h95'
 
 DATABASES = {
     'default': {
