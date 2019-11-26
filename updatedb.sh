2a2f2c9414e74ed971bf3c4d2634377c3803c86b
sandrinesuire <suiresandrine1@gmail.com>
2019-11-26 17:57:02 +0100

    feat(nutella command) add command to update database from cron task

--- updatedb.sh
+++ updatedb.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd /home/sandrinesuire/project_10/substitute/management/commands
-source ../../../../venv/bin/activate
+source ../../../../env/bin/activate
 python ../../../../manage.py updatedb
