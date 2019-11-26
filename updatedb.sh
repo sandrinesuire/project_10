9c612ecf6c65b03ca1ee6a75e82a872b2a7022ce
suire <suiresandrine1@gmail.com>
2019-11-26 19:40:53 +0000

    merge

--- updatedb.sh
+++ updatedb.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd /home/sandrinesuire/project_10/substitute/management/commands
 source ../../../../env/bin/activate
+export DJANGO_SETTINGS_MODULE="nutella.settings.production"
 python ../../../manage.py updatedb
