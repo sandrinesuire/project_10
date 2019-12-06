66bb869f180822845061022a0ebc7670abfc3bb8
sandrinesuire <suiresandrine1@gmail.com>
2019-12-06 16:27:14 +0100

    test (settings file)

--- updatedb.sh
+++ updatedb.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/sandrinesuire/project_10/substitute/management/commands
-source ../../../../venv/bin/activate
+source ../../../../env/bin/activate
 export DJANGO_SETTINGS_MODULE="nutella.settings.production"
 python ../../../manage.py updatedb
