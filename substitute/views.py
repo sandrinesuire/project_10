036f60c8f0c7b28b7aebe17e174acecb501b4581
sandrinesuire <suiresandrine1@gmail.com>
2019-12-07 14:56:07 +0100

    feat (store ip user) add agent new relic to store ip user

--- substitute/views.py
+++ substitute/views.py
@@ -137,11 +137,22 @@ def search(request):
     """
     form = SearchForm(request.POST or None)
     if request.method == "POST" and form.is_valid():
+        user_ip = get_client_ip(request)
+        agent.add_custom_parameter('user_ip', user_ip)
         searching = form.cleaned_data["searching"]
         context = _search(searching)
         return render(request, 'substitute/results.html', context)
 
     return render(request, 'substitute/search.html', {'form': form})
+
+
+def get_client_ip(request):
+    x_forwarded_for = request.META.get('HTTP_X_FORWARDED_FOR')
+    if x_forwarded_for:
+        ip = x_forwarded_for.split(',')[0]
+    else:
+        ip = request.META.get('REMOTE_ADDR')
+    return ip
 
 
 def _search(searching):
