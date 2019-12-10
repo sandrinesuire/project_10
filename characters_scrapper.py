e7d2d6b6c9c7ee50ea882b65adfa72dab7b9c720
sandrinesuire <suiresandrine1@gmail.com>
2019-12-10 13:31:16 +0100

    test scrapy ()

--- characters_scrapper.py
+++ characters_scrapper.py
@@ -1,42 +1,42 @@
+# This Python file uses the following encoding: utf-8
 import scrapy
 
 class BlogSpider(scrapy.Spider):
     name = 'characterspider'
-    start_urls = ['https://www.aroma-zone.com/info/guide-des-huiles-essentielles/tous?tid_problematique=519']
+    start_urls = [
+        'https://www.aroma-zone.com/info/guide-des-huiles-essentielles/tous',
+    ]
 
     def parse(self, response):
-        for link in response.css('div.select-filter li'):
-            yield {'character': link.css('a ::text').extract_first(),
-                   'link': link.css('a::attr(href)').extract_first()
-                   }
+        for next_page in response.css('div.select-filter li'):
+            if next_page is not None:
+                title = next_page.css('a ::text').extract()
+                url = next_page.css('a::attr(href)').extract_first()
+                next_page = response.urljoin(url)
+                yield scrapy.Request(next_page, callback=self.parse, meta={"title": title})
 
-            # for link in response.css('div.view-content div'):
-            #     yield {'character': link.css('a::attr(title)').extract_first(),
-            #            'link': link.css('a::attr(href)').extract_first()
-            #            }
-            
-            hxs = HtmlXPathSelector(response)
-            sites = hxs.select('//table[@class="custom_table"]/tr')
+        for link in response.css('div.products compact'):
+            if link.css('a::attr(href)').extract_first() is not None:
+                meta = response.meta
+                url1 = link.css('a::attr(href)').extract_first()
+                next_page1 = response.urljoin(url1)
+                meta["character"] = link.css('a::attr(title)').extract_first()
+                yield scrapy.Request(next_page1, callback=self.parse, meta=meta)
+
+        for link1 in response.css('div.propriete-desc proprietes li'):
+            if link1.css('a::attr(href)').extract_first() is not None:
+                meta = response.meta
+                yield {'title': meta["title"],
+                       'character': meta["character"],
+                       'properties': "essai"
+                       }
 
 
-
-
-
-            items = []
-
-            for site in sites:
-
-                #print site
-                item = DeloitteListingItem()
-
-                name = ''.join(site.select('./td/a/text()').extract())
-                url = ''.join(site.select('./td/a/@href').extract())
-                ca = ''.join(site.select('./td[4]/text()').extract())
-
-                item['name'] = name
-                item['url'] = url
-                item['ca'] = ca
-
-                items.append(item)
-
-            return items
+        # for link in response.css('div.select-filter li'):
+        #     yield {'character': link.css('a ::text').extract_first(),
+        #            'link': link.css('a::attr(href)').extract_first()
+        #            }
+        # for link in response.css('div.view-content div'):
+        #     yield {'character': link.css('a::attr(title)').extract_first(),
+        #            'link': link.css('a::attr(href)').extract_first()
+        #            }
